18f0938cc44bed0209902f3709ec471ebd67dff4
valeran86 <valeran86@gmail.com>
2018-09-01 02:32:19 -0400

    correct init containers

--- rebuild.sh
+++ rebuild.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 docker-compose down
-docker rm -f spring-hiber-container
+#docker rm -f spring-hiber-container
 docker rmi -f spring-hiber-image
-mvn clean package
+mvn clean package -DskipTests=true
 docker build . -t spring-hiber-image
 #docker run -p 8080:8080 --name spring-hiber-container spring-hiber-image
 #docker logs -f spring-hiber-container
